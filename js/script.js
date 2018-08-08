387f5e56febc372a68fbcbf829831e28cd449bc6
FutureBoyfriend <41453514+FutureBoyfriend@users.noreply.github.com>
2018-08-08 15:37:01 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
+  $(".item").hide();
   $("#logo").fadeIn(600);
   $(".text").fadeIn(700);
   $("#insta").fadeIn(800);
   $("#follow").fadeIn(900);
+  $(".a").fadeIn(800);
+  $(".b").fadeIn(825);
+  $(".c").fadeIn(850);
+  $(".d").fadeIn(875);
 })
