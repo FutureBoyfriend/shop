596734b3c3eb80648b22f819e6d5f99a2f05de4c
FutureBoyfriend <41453514+FutureBoyfriend@users.noreply.github.com>
2018-08-29 16:19:00 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -46,5 +46,9 @@ $(document).ready(function() {
   })
 
   var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
-  console.log(isSafari);
+
+  if (isSafari) {
+    $(".other").addClass("hidden");
+    $(".safari").removeClass("hidden");
+  }
 })
