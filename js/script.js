38691784fe518e33b8aafba085a3a293455c84bf
FutureBoyfriend <41453514+FutureBoyfriend@users.noreply.github.com>
2018-08-29 15:19:24 -0400

    Fixed mobile bug

--- js/script.js
+++ js/script.js
@@ -8,4 +8,40 @@ $(document).ready(function() {
   $(".b").fadeIn(825);
   $(".c").fadeIn(850);
   $(".d").fadeIn(875);
+
+  function count(amount, label) {
+    var n = {"amount": amount, "label": label}
+    $.ajax({
+      data: n,
+      dataType: "json",
+      url: "../py/count.py",
+      success: function(result, status, xhr) {
+        console.log(result);
+      },
+      error: function(xhr, status, error) {
+        console.log(error);
+        console.log(status);
+      }
+    })
+  }
+
+  $("#a").click(function() {
+    count(5, "bf post");
+  })
+
+  $("#b").click(function() {
+    count(10, "bf highlight");
+  })
+
+  $("#c").click(function() {
+    count(10, "promo1");
+  })
+
+  $("#d").click(function() {
+    count(15, "promo2");
+  })
+
+  $(document).bind("mobileinit", function() {
+    $.mobile.ajaxEnabled = false;
+  })
 })
