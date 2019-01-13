b1ae88caf3c243ee5a843b4778b7205a7c48aa50
FutureBoyfriend <41453514+FutureBoyfriend@users.noreply.github.com>
2019-01-13 17:17:48 -0500

    Added dropdown options

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@ $(document).ready(function() {
   $(".c").fadeIn(850);
   $(".d").fadeIn(875);
   $(".e").fadeIn(900);
+  $(".opt2").hide();
+  $(".opt4").hide();
 
   $(document).bind("mobileinit", function() {
     $.mobile.ajaxEnabled = false;
@@ -40,5 +42,54 @@ $(document).ready(function() {
 			}
 		}
 	  ]
+  });
+  
+  $("#postDropdown").change(function() {
+	 var selected = $(".dropdown :selected").text();
+	 if (selected.charAt(0) == '1') {
+		$("#postPrice").text("$5");
+	 }
+	 else {
+		 $("#postPrice").text("$12");
+	 }
+  });
+  
+  $("#accountDropdown").change(function() {
+	 var selected = $("#accountDropdown :selected").text();
+	 if (selected.charAt(0) == '3') {
+		$("#accountPrice").text("$15");
+	 }
+	 else {
+		 $("#accountPrice").text("$20");
+	 }
+  });
+  
+  
+  $("#safariPostDropdown").change(function() {
+	 var selected = $("#safariPostDropdown :selected").text();
+	 if (selected.charAt(0) == '1') {
+		$("#safariPostPrice").text("$5");
+		$(".opt2").hide();
+		$(".opt1").show();
+	 }
+	 else {
+		 $("#safariPostPrice").text("$12");
+		 $(".opt1").hide();
+		 $(".opt2").show();
+	 }
+  });
+  
+  $("#safariAccountDropdown").change(function() {
+	 var selected = $("#safariAccountDropdown :selected").text();
+	 if (selected.charAt(0) == '3') {
+		$("#safariAccountPrice").text("$15");
+		$(".opt4").hide();
+		$(".opt3").show();
+	 }
+	 else {
+		 $("#safariAccountPrice").text("$20");
+		 $(".opt3").hide();
+		 $(".opt4").show();
+	 }
   });
 })
